828e589d4e2031612d4f5d10d28e05a4b1da780b
Fabio Stratotti <fabio_stratotti@hotmail.com>
2025-03-26 23:04:57 -0300

    Adicionado json-server para ser nossa fake API.

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,23 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Esse é um pensamento para realizar o teste de formatação. Queremos validar se quando temos um pensamento maior que o tamanho pré-definido(256 caracteres). Com isso precisamos então colocar um pensamento bem mais longo, para que o car do pensamento tenha uma largura maior, assim conseguiremos validar as formatações e entender melhor o como vai funcionar o esquema...',
-      autoria: 'Formatador',
-      modelo: 'modelo1'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
+  // listaPensamentos: Pensamento[] = [
+  //   {
+  //     conteudo: 'Passo informações para o componente filho',
+  //     autoria: 'Componente pai',
+  //     modelo: 'modelo3'
+  //   },
+  //   {
+  //     conteudo: 'Minha propriedade é decorada com @Input',
+  //     autoria: 'Componente filho',
+  //     modelo: 'modelo2'
+  //   },
+  //   {
+  //     conteudo: 'Esse é um pensamento para realizar o teste de formatação. Queremos validar se quando temos um pensamento maior que o tamanho pré-definido(256 caracteres). Com isso precisamos então colocar um pensamento bem mais longo, para que o car do pensamento tenha uma largura maior, assim conseguiremos validar as formatações e entender melhor o como vai funcionar o esquema...',
+  //     autoria: 'Formatador',
+  //     modelo: 'modelo1'
+  //   }
+  // ];
 
   constructor() { }
 
